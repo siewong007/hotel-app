9bf7b1830da9e90e443dfda56ea49e34c7b6edc6
siewong007 <siewong007@gmail.com>
2026-07-23 17:25:02 +0800

    fix(communications): support hmac 0.13 key initialization

--- hotel-app-be/src/modules/communications/tokens.rs
+++ hotel-app-be/src/modules/communications/tokens.rs
@@ -6,7 +6,7 @@
 
 use base64::Engine;
 use base64::engine::general_purpose::URL_SAFE_NO_PAD;
-use hmac::{Hmac, Mac};
+use hmac::{Hmac, KeyInit, Mac};
 use sha2::Sha256;
 
 use crate::core::error::ApiError;
